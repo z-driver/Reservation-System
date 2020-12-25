5d128951fa341b17bffb5c2637b948b7a210354f
z-driver <1027319981@qq.com>
2020-12-25 10:51:32 +0800

    dbo
    修改了数据库

--- dbo/dbo.ReservationRecord.sql
+++ dbo/dbo.ReservationRecord.sql
@@ -2,8 +2,10 @@
 (
 	[student_id] INT NOT NULL , 
     [room_id] INT NOT NULL, 
-	[number] NCHAR(10) NOT NULL DEFAULT 1, --预约人数
+	[number] INT NOT NULL DEFAULT 1, --预约人数
     [start_time] DATETIME NOT NULL, 
 	[end_time] DATETIME NOT NULL, 
-    PRIMARY KEY ([student_id],[room_id])
+    PRIMARY KEY ([student_id],[room_id],[start_time]),
+	FOREIGN KEY ([student_id]) REFERENCES StudentLogin([student_id]),
+	FOREIGN KEY ([room_id]) REFERENCES SeminarRoom([room_id])
 )
---
+++ dbo/index.StudentHistory.sql
@@ -0,0 +1,1 @@
+﻿CREATE INDEX StudentHistory ON ReservationRecord([student_id],[room_id],[number],[start_time],[end_time])
